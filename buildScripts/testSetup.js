add0cbca94851dad1105698a712ef1da799201a9
Anai Araya <anai.yusary@gmail.com>
2018-06-21 15:30:22 +1000

    add mocha script

---
+++ buildScripts/testSetup.js
@@ -0,0 +1,8 @@
+//this file is not transpiled, so must use CommonJS and ES5
+
+// Register babel to transpile before our tests run
+require('babel-register')()
+
+// Disable webpack features that mocha doesnt understand.
+require.extensions('.css') = function() {}
+
