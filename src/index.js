2d3e5a5c066f5af25f8876262b0b283195f585dc
Anai Araya <anai.yusary@gmail.com>
2018-06-25 16:27:01 +1000

    add logic to activate delete buttons

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import {getUsers} from './api/userApi'
+import {getUsers, deleteUser} from './api/userApi'
 
 // * Populate table of users using via API call
 getUsers().then(result => {
@@ -15,20 +15,18 @@ getUsers().then(result => {
   })
 
   global.document.getElementById('users').innerHTML = usersBody
-})
-
-/*
   const deleteLinks = global.document.getElementsByClassName('deleteUser');
 
   // Must use array.from to create a real array from a DOM collection
   // getElementsByClassname only returns an "array like" object
   Array.from(deleteLinks, link => {
     link.onclick = function (event) {
-      const element = event.target
-      event.preventDefault()
-      deleteUser(element.attributes["data-id"].value)
-      const row = element.parentNode.parentNode
-      row.parentNode.removeChild(row)
+      const element = event.target;
+      event.preventDefault();
+      deleteUser(element.attributes["data-id"].value);
+      const row = element.parentNode.parentNode;
+      row.parentNode.removeChild(row);
     }
   })
-*/
+
+})
