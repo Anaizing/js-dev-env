47b78f9325ae246c9f35b10aa203a085149ade32
Anai Araya <anai.yusary@gmail.com>
2018-06-30 19:33:45 +1000

    add webpack html plugin

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,5 +1,6 @@
 import webpack from 'webpack'
 import path from 'path'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
 
 export default {
   mode: 'production',
@@ -14,6 +15,13 @@ export default {
     filename: 'bundle.js'
   },
   plugins: [
+
+    // * Create HTML file that includes reference to bundled JS
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      inject: true
+    }),
+
     new webpack.LoaderOptionsPlugin({
       debug: true,
       noInfo: false,
